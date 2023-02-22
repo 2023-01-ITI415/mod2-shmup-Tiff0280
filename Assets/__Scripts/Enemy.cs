d78a562a9b98ffd6670572e7d20b89738643d548
Tiff0280 <ty194@scarletmail.rutgers.edu>
2023-02-22 17:35:29 -0500

    Added Enemy Script

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Header("Inscribed")]
+    public float speed = 10f;   // The movement speed is 10m/s
+    public float fireRate = 0.3f;  // Seconds/shot (Unused)
+    public float health = 10;    // Damage needed to destroy this enemy
+    public int score = 100;   // Points earned for destroying this
+ 
+    // This is a Property: A method that acts like a field
+    public Vector3 pos
+    {                                                      
+        get
+        {
+            return this.transform.position;
+        }
+        set
+        {
+            this.transform.position = value;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();                                                              
+    }
+
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
     }
 }
